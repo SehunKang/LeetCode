f44a84bbf53054d97cee986647b5905f0b7825e0
Sehun Kang <andriy.kang@gmail.com>
2022-03-24 20:38:47 +0900

    Time: 25 ms (60.35%), Space: 14.5 MB (32.68%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.swift
+++ 13-roman-to-integer/13-roman-to-integer.swift
@@ -1,70 +1,25 @@
 class Solution {
-    func romanToInt(_ s: String) -> Int {
-        var totalNum = 0
-        let charArray: [String] = stringToArray(s)
-        var index = 0
-        
-        while index < charArray.count {
-            let roman = charArray[index]
-            if roman == "I" {
-                if index + 1 < charArray.count {
-                    if charArray[index + 1] == "V" {
-                        totalNum += 4
-                        index += 2
-                        continue
-                    } else if charArray[index + 1] == "X" {
-                        totalNum += 9
-                        index += 2
-                        continue
-                    }
-                }
-                totalNum += 1
-            } else if roman == "V" {
-                totalNum += 5
-            } else if roman == "X" {
-                if index + 1 < charArray.count {
-                    if charArray[index + 1] == "L" {
-                        totalNum += 40
-                        index += 2
-                        continue
-                    } else if charArray[index + 1] == "C" {
-                        totalNum += 90
-                        index += 2
-                        continue
-                    }
-                }
-                totalNum += 10
-            } else if roman == "L" {
-                totalNum += 50
-            } else if roman == "C" {
-                if index + 1 < charArray.count {
-                    
-                    if charArray[index + 1] == "D" {
-                        totalNum += 400
-                        index += 2
-                        continue
-                    } else if charArray[index + 1] == "M" {
-                        totalNum += 900
-                        index += 2
-                        continue
-                    }
-                }
-                totalNum += 100
-            } else if roman == "D" {
-                totalNum += 500
-            } else if roman == "M" {
-                totalNum += 1000
-            }
-                index += 1
-        }
-        return totalNum
-    }
     
-    func stringToArray(_ s: String) -> [String] {
-        var charArray: [String] = []
+    let roman = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
+
+    func romanToInt(_ s: String) -> Int {
+        var prev = 0
+        var result = 0
+        
         for i in s {
-            charArray.append(i.description)
+            let val = roman[i.description]!
+            if val > prev {
+                result -= prev
+                result += -(prev - val)
+            } else {
+                result += val
+            }
+            prev = val
         }
-        return charArray
+        return result
     }
 }
+
+let c = Solution()
+
+c.romanToInt("MCMXCIV")
