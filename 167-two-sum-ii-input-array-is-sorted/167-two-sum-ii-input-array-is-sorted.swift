556a33c7f4db9b67a439c30aef3911133fcf9207
Sehun Kang <andriy.kang@gmail.com>
2022-03-30 13:28:09 +0900

    Time: 81 ms (66.86%), Space: 14.5 MB (88.07%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.swift
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.swift
@@ -1,12 +1,14 @@
 class Solution {
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
-        for i in numbers.indices {
-            for j in i + 1..<numbers.count {
-                if numbers[i] + numbers[j] == target {
-                    return [i + 1, j + 1]
-                }
+        var l = 0
+        var r = numbers.count - 1
+        while numbers[l] + numbers[r] != target {
+            if numbers[l] + numbers[r] < target {
+                l += 1
+            } else {
+                r -= 1
             }
         }
-        return [0]
+        return [l + 1, r + 1]
     }
 }
