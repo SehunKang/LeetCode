f476a37a79612568b8e1c4552f4ef32d5d68f409
Sehun Kang <andriy.kang@gmail.com>
2022-04-01 08:52:24 +0900

    Time: 52 ms (74.39%), Space: 14.2 MB (83.18%) - LeetHub

--- 1-two-sum/1-two-sum.swift
+++ 1-two-sum/1-two-sum.swift
@@ -1,15 +1,14 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        let num = nums.sorted()
-        var left = 0
-        var right = num.count - 1
-        while num[left] + num[right] != target {
-            if num[left] + num[right] < target {
-                left += 1
-            } else {
-                right -= 1
+        var dict = [Int: Int]()
+        
+        for (index, value) in nums.enumerated() {
+            if dict[target - value] != nil {
+                return [dict[target - value]!, index]
             }
+            
+            dict[value] = index
         }
-        return [nums.firstIndex(of: num[left])!, nums.lastIndex(of: num[right])!]
+        return [0]
     }
 }
