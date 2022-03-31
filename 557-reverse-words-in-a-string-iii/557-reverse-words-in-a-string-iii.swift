6c352bf667a8f866a2a24b53de9960a0351adc86
Sehun Kang <andriy.kang@gmail.com>
2022-03-31 12:12:37 +0900

    Time: 367 ms (13.22%), Space: 15.3 MB (56.03%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.swift
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.swift
@@ -1,7 +1,5 @@
 class Solution {
     func reverseWords(_ s: String) -> String {
-        var c = String(s.split(separator: " ").map { $0.reversed() }.flatMap {$0 + " "})
-        c.removeLast()
-        return c
+        String(s.split(separator: " ").map {$0.reversed()}.joined(separator: " "))
     }
 }
