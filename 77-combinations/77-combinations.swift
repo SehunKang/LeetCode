1339355e0d29c0b75f9218a0d98185f8848e2dfb
Sehun Kang <andriy.kang@gmail.com>
2022-04-08 11:03:42 +0900

    Time: 187 ms (14.28%), Space: 16 MB (52.14%) - LeetHub

--- 77-combinations/77-combinations.swift
+++ 77-combinations/77-combinations.swift
@@ -1,28 +1,25 @@
 class Solution {
-    private func recurse( result: inout [[Int]], element: inout [Int], start: Int, n: Int, k: Int) {
-        if k == 0 {
-            result.append(element)
-            return
-        }
-        
-        if start > n {
-            return
-        }
-        
-        
-        for i in start...n {
-            element.append(i)
-            recurse(result: &result, element: &element, start: i + 1, n: n, k: k - 1)
-            element.removeLast()
-        }
-        
-    }
-    
     func combine(_ n: Int, _ k: Int) -> [[Int]] {
         var result: [[Int]] = []
-        var element: [Int] = []
         
-        recurse(result: &result, element: &element, start: 1, n: n, k: k)
+        func recurse(element: [Int], start: Int) {
+            if element.count == k {
+                result.append(element)
+                return
+            }
+            
+            var element = element
+            
+            if start > n { return }
+            
+            for i in start...n {
+                element.append(i)
+                recurse(element: element, start: i + 1)
+                element.removeLast()
+            }
+        }
+        
+        recurse(element: [], start: 1)
         return result
     }
 }
