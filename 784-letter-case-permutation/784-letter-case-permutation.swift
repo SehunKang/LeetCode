be78b35376a893acd6031ee38fd4f5127f433dec
Sehun Kang <andriy.kang@gmail.com>
2022-04-10 15:11:13 +0900

    Time: 99 ms (47.32%), Space: 14.7 MB (80.36%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.swift
+++ 784-letter-case-permutation/784-letter-case-permutation.swift
@@ -1,23 +1,29 @@
 class Solution {
-    func letterCasePermutation(_ S: String) -> [String] {
-        var result = [S]
-        let String = S.map { $0 }
-        
-        for i in 0..<S.count {
-            if String[i].isNumber { continue }
-            let size = result.count
-            for j in 0..<size {
-                var currentString = result.first!.map { $0.description }
-                result.removeFirst()
-                
-                currentString[i] = currentString[i].uppercased()
-                result.append(currentString.joined())
-                
-                currentString[i] = currentString[i].lowercased()
-                result.append(currentString.joined())
-            }
+    private func dfs(result: inout [String], chars: inout [String], index: Int) {
+        if index == chars.count {
+            result.append(chars.joined())
+            return
         }
         
+        if Character(chars[index]).isNumber {
+            dfs(result: &result, chars: &chars, index: index + 1)
+            return
+        }
+        
+        chars[index] = chars[index].lowercased()
+        dfs(result: &result, chars: &chars, index: index + 1)
+        
+        chars[index] = chars[index].uppercased()
+        dfs(result: &result, chars: &chars, index: index + 1)
+    }
+    
+    func letterCasePermutation(_ S: String) -> [String] {
+        var result = [String]()
+        var chars = S.map { $0.description }
+        
+        dfs(result: &result, chars: &chars, index: 0)
+        
         return result
+        
     }
 }
