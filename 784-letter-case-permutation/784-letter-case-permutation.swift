e36c3503288f7ba0c0bcf3fd59f3a4cf79f8cfc1
Sehun Kang <andriy.kang@gmail.com>
2022-04-10 15:21:45 +0900

    Time: 79 ms (66.96%), Space: 15.1 MB (39.29%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.swift
+++ 784-letter-case-permutation/784-letter-case-permutation.swift
@@ -1,29 +1,14 @@
 class Solution {
-    private func dfs(result: inout [String], chars: inout [String], index: Int) {
-        if index == chars.count {
-            result.append(chars.joined())
-            return
-        }
-        
-        if Character(chars[index]).isNumber {
-            dfs(result: &result, chars: &chars, index: index + 1)
-            return
-        }
-        
-        chars[index] = chars[index].lowercased()
-        dfs(result: &result, chars: &chars, index: index + 1)
-        
-        chars[index] = chars[index].uppercased()
-        dfs(result: &result, chars: &chars, index: index + 1)
-    }
-    
     func letterCasePermutation(_ S: String) -> [String] {
-        var result = [String]()
-        var chars = S.map { $0.description }
+        var result: [String] = [""]
         
-        dfs(result: &result, chars: &chars, index: 0)
-        
+        for char in S {
+            if char.isLetter {
+                result = result.flatMap { ["\($0)\(char.lowercased())", "\($0)\(char.uppercased())"] }
+            } else {
+                result = result.map { "\($0)\(char)" }
+            }
+        }
         return result
-        
     }
 }
