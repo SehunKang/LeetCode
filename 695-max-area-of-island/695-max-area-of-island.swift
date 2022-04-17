6fb93577b377bca85535380440312b7fedb093c3
Sehun Kang <andriy.kang@gmail.com>
2022-04-17 09:40:50 +0900

    Time: 68 ms (79.20%), Space: 14.5 MB (51.60%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.swift
+++ 695-max-area-of-island/695-max-area-of-island.swift
@@ -1,34 +1,32 @@
 class Solution {
-
-    private func search(grid: inout [[Int]], target: [Int], size: inout Int) {
-        if target[0] < 0 || target[0] >= grid.count
-            || target[1] < 0 || target[1] >= grid[0].count { return }
-        if grid[target[0]][target[1]] != 1 { return }
-
-        if grid[target[0]][target[1]] == 1 {
-            grid[target[0]][target[1]] = 2
-            size += 1
-        }
-        search(grid: &grid, target: [target[0] - 1, target[1]], size: &size)
-        search(grid: &grid, target: [target[0] + 1, target[1]], size: &size)
-        search(grid: &grid, target: [target[0], target[1] - 1], size: &size)
-        search(grid: &grid, target: [target[0], target[1] + 1], size: &size)
-
-
+    
+    private func measureIsland(size: inout Int, grid: inout [[Int]], src: [Int]){
+        if grid[src[0]][src[1]] != 1 { return }
+        
+        grid[src[0]][src[1]] = 2
+        size += 1
+        
+        if src[0] != 0 { measureIsland(size: &size, grid: &grid, src: [src[0] - 1, src[1]])}
+        if src[0] != grid.count - 1 { measureIsland(size: &size, grid: &grid, src: [src[0] + 1, src[1]])}
+        if src[1] != 0 { measureIsland(size: &size, grid: &grid, src: [src[0], src[1] - 1])}
+        if src[1] != grid[0].count - 1 { measureIsland(size: &size, grid: &grid, src: [src[0], src[1] + 1])}
+        
     }
 
     func maxAreaOfIsland(_ grid: [[Int]]) -> Int {
         var grid = grid
         var result = 0
-        for i in grid.indices {
-            for j in grid[0].indices {
+        
+        for i in 0..<grid.count {
+            for j in 0..<grid[0].count {
+                var islandSize = 0
                 if grid[i][j] == 1 {
-                    var size = 0
-                    search(grid: &grid, target: [i, j], size: &size)
-                    result = size > result ? size : result
+                    measureIsland(size: &islandSize, grid: &grid, src: [i, j])
+                    result = max(result, islandSize)
                 }
             }
         }
+        
         return result
     }
 }
