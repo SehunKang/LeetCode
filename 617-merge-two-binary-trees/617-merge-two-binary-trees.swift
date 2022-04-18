e31ba69ae88733e259579be9b5ceb35494ebc9e3
Sehun Kang <andriy.kang@gmail.com>
2022-04-18 09:39:49 +0900

    Time: 95 ms (75.39%), Space: 14.3 MB (95.70%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.swift
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.swift
@@ -15,11 +15,12 @@
  */
 class Solution {
     func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
-        if root1 == nil { return root2}
-        if root2 == nil { return root1}
-        root1!.val += root2!.val
+        if root1 == nil {return root2}
+        if root2 == nil {return root1}
+        root1?.val += root2!.val
         root1?.left = mergeTrees(root1?.left, root2?.left)
         root1?.right = mergeTrees(root1?.right, root2?.right)
+        
         return root1
     }
 }
