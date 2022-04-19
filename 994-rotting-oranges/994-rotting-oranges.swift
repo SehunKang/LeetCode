30d62b7c8922738502cea80b703da5d6da409a6d
Sehun Kang <andriy.kang@gmail.com>
2022-04-19 23:27:06 +0900

    Time: 25 ms (34.29%), Space: 14.3 MB (17.14%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.swift
+++ 994-rotting-oranges/994-rotting-oranges.swift
@@ -1,60 +1,45 @@
 class Solution {
+    private func getRotten(grid: inout [[Int]], queue: inout [[Int]], x: Int, y: Int, fresh: inout Int) {
+        if x < 0 || y < 0 || x > grid.count - 1 || y > grid[0].count - 1 { return }
+        
+        if grid[x][y] == 1 {
+            grid[x][y] = 2
+            fresh -= 1
+            queue.append([x, y])
+        }
+    }
+    
     func orangesRotting(_ grid: [[Int]]) -> Int {
+        let xy = [0, 1, 0, -1, 0]
         var grid = grid
-        var result = -1
-        var q: [[Int]] = []
         var fresh = 0
+        var result = -1
+        var queue: [[Int]] = []
         
         for i in grid.indices {
             for j in grid[0].indices {
                 if grid[i][j] == 2 {
-                    q.append([i, j])
-                } else if grid[i][j] == 1 {
+                    queue.append([i, j])
+                }
+                if grid[i][j] == 1 {
                     fresh += 1
                 }
             }
         }
-        if q.isEmpty && fresh == 0 { return 0}
-        if fresh == 0 { return 0}
         
-        while !q.isEmpty {
+        if fresh == 0 { return 0 }
+        
+        while !queue.isEmpty {
             result += 1
-            let size = q.count
-            print(q)
-            for _ in 1...size {
-                let target = q.first!
-                q.removeFirst()
-                if target[0] > 0 {
-                    if grid[target[0] - 1][target[1]] == 1 {
-                        q.append([target[0] - 1, target[1]])
-                        grid[target[0] - 1][target[1]] = 2
-                        fresh -= 1
-                    }
-                }
-                if target[0] < grid.count - 1 {
-                    if grid[target[0] + 1][target[1]] == 1 {
-                        q.append([target[0] + 1, target[1]])
-                        grid[target[0] + 1][target[1]] = 2
-                        fresh -= 1
-                    }
-                    
-                }
-                if target[1] > 0 {
-                    if grid[target[0]][target[1] - 1] == 1 {
-                        q.append([target[0], target[1] - 1])
-                        grid[target[0]][target[1] - 1] = 2
-                        fresh -= 1
-                    }
-                }
-                if target[1] < grid[0].count - 1 {
-                    if grid[target[0]][target[1] + 1] == 1 {
-                        q.append([target[0], target[1] + 1])
-                        grid[target[0]][target[1] + 1] = 2
-                        fresh -= 1
-                    }
+            let count = queue.count
+            for _ in 0..<count {
+                let target = queue.first!
+                queue.removeFirst()
+                for i in 0...3 {
+                    getRotten(grid: &grid, queue: &queue, x: target[0] + xy[i], y: target[1] + xy[i + 1], fresh: &fresh)
                 }
             }
         }
-        return fresh == 0 ? result : -1
+        return fresh != 0 ? -1 : result
     }
 }
