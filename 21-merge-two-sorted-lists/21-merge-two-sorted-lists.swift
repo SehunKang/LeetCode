4766ffd9a080a0a12dfd4aaf06c9c940886dc057
Sehun Kang <andriy.kang@gmail.com>
2022-04-20 10:22:32 +0900

    Time: 11 ms (77.34%), Space: 14.1 MB (56.35%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.swift
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.swift
@@ -10,20 +10,20 @@
  */
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        
         if list1 == nil {
             return list2
         } else if list2 == nil {
             return list1
         }
-        
-        if (list1?.val)! < (list2?.val)! {
+
+        if list1!.val < list2!.val {
             list1?.next = mergeTwoLists(list1?.next, list2)
             return list1
         } else {
             list2?.next = mergeTwoLists(list1, list2?.next)
             return list2
         }
-        
+
+
     }
 }
