240a9e19cd030e93ebdb110681c72cd0690b5e36
Sehun Kang <andriy.kang@gmail.com>
2022-04-21 10:03:53 +0900

    Time: 325 ms (5.45%), Space: 14.7 MB (79.09%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.swift
+++ 784-letter-case-permutation/784-letter-case-permutation.swift
@@ -1,15 +1,30 @@
 class Solution {
-    func letterCasePermutation(_ s: String) -> [String] {
-        var result: [String] = [""]
+    private func task(index: Int ,currentStr: String, chars: [Character], result: inout [String]) {
         
-        for char in s {
-            if char.isLetter {
-                result = result.flatMap {["\($0)\(char.lowercased())","\($0)\(char.uppercased())"]}
-            } else {
-                result = result.map {"\($0)\(char)"}
-            }
+        if currentStr.count == chars.count {
+            result.append(currentStr)
+            return
         }
         
+        if index > chars.count - 1 { return}
+        
+        for i in index...chars.count - 1 {
+            if chars[i].isLetter {
+                task(index: i + 1, currentStr: currentStr + chars[i].lowercased(), chars: chars, result: &result)
+                task(index: i + 1, currentStr: currentStr + chars[i].uppercased(), chars: chars, result: &result)
+            } else {
+                task(index: i + 1, currentStr: currentStr + String(chars[i]), chars: chars, result: &result)
+            }
+            
+        }
+        
+    }
+    
+    func letterCasePermutation(_ s: String) -> [String] {
+        var result = [String]()
+        let chars = Array(s)
+        
+        task(index: 0, currentStr: "", chars: chars, result: &result)
         return result
     }
 }
