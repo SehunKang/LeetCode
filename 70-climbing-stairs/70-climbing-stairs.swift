a5c9453756880917115951f12e827f6fa5e3521a
Sehun Kang <andriy.kang@gmail.com>
2022-04-22 09:34:14 +0900

    Time: 10 ms (5.51%), Space: 13.3 MB (97.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.swift
+++ 70-climbing-stairs/70-climbing-stairs.swift
@@ -1,18 +1,18 @@
 class Solution {
-    var cash: [Int: Int] = [1: 1, 2: 2]
 
     func climbStairs(_ n: Int) -> Int {
-
-        var result = 0
-
-        if cash[n] != nil {
-            return cash[n]!
+        
+        if n == 1 {return 1}
+        
+        var arr: [Int] = Array(repeating: 0, count: n + 1)
+        
+        arr[0] = 1
+        arr[1] = 1
+        
+        for i in 2...n {
+            arr[i] = arr[i - 2] + arr[i - 1]
         }
+        return arr[n]
 
-        result += climbStairs(n - 1)
-        result += climbStairs(n - 2)
-
-        cash[n] = result
-        return result
     }
 }
