fe36e89e04074a2ab02d10fe76e94520a558ffd1
Sehun Kang <andriy.kang@gmail.com>
2022-05-01 21:47:57 +0900

    Time: 17 ms (85.04%), Space: 14.5 MB (69.29%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.swift
+++ 242-valid-anagram/242-valid-anagram.swift
@@ -1,14 +1,10 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        if t.count != s.count { return false }
-        var t = t
-        for char in s {
-            if t.contains(char) {
-                t.remove(at: t.firstIndex(of: char)!)
-                continue
-            }
-            return false
-        }
-        return true
+        var dict = [Character: Int]()
+        
+        s.forEach { dict[$0, default: 0] += 1}
+        t.forEach { dict[$0, default: 0] -= 1}
+        
+        return dict.values.filter {$0 != 0}.count == 0
     }
 }
