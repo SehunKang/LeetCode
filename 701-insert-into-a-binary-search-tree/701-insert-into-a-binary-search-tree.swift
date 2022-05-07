573fd6af7fb97f9f8262b1b8855d46c56d872bf6
Sehun Kang <andriy.kang@gmail.com>
2022-05-07 11:46:52 +0900

    Time: 267 ms (29.59%), Space: 15.6 MB (22.45%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.swift
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.swift
@@ -14,32 +14,17 @@
  * }
  */
 class Solution {
-    private func addNode(node: TreeNode?, val: Int) {
-        guard let node = node else {
-            return
-        }
-        if node.val > val {
-            if node.left != nil {
-                addNode(node: node.left, val: val)
-            } else {
-                node.left = TreeNode(val)
-            }
-        } else {
-            if node.right != nil {
-                addNode(node: node.right, val: val)
-            } else {
-                node.right = TreeNode(val)
-            }
-        }
-
-        
-    }
-    
     func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
-        guard let root = root else {
+        guard var root = root else {
             return TreeNode(val)
         }
-        addNode(node: root, val: val)
+        
+        if root.val > val {
+            root.left = insertIntoBST(root.left, val)!
+        } else {
+            root.right = insertIntoBST(root.right, val)!
+        }
+        
         return root
     }
 }
